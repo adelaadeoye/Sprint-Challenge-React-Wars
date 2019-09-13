8206c42c6c77ae7e85d95415caeef496b977d118
adeoye adela <adeoye.adela@gmail.com>
2019-09-13 21:43:47 +0100

    Got the data display accurately

--- starwars/src/components/dataComponent/ContentTable.js
+++ starwars/src/components/dataComponent/ContentTable.js
@@ -1,163 +1,83 @@
 import React from 'react';
 import { MDBDataTable } from 'mdbreact';
-import { Table } from 'reactstrap';
 
-export default  function Example (props) {
-      return (
-        <Table key={props.name}>
-          <thead>
-            <tr>
-              <th>S/No</th>
-              <th>Name</th>
-              <th>Birth Year</th>
-              <th>Eye Color</th>
-              <th>Skin Color</th>
-              <th>Height</th>
-              <th>Mass</th>
-              <th>Films Acted In</th>
-              <th>No of Species</th>
-              <th>No of Starships</th>
-              <th>No of Vehicles</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>{props.name}</td>
-              <td>{props.birthYear}</td>
-              <td>{props.eyeColor}</td>
-              <td>{props.skinColor}</td>
-              <td>{props.height}</td>
-              <td>{props.mass}</td>
-              <td>{props.films}</td>
-              <td>{props.species}</td>
-              <td>{props.starships}</td>
-              <td>{props.vehicles}</td>
-            </tr>
-            
-          </tbody>
-        </Table>
-      );
-    }
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const DatatablePage = props => {
+const DatatablePage = props => {
     
-//   const data = {
-//     columns: [
-//       {
-//         label: 'Name',
-//         field: 'name',
-//         sort: 'asc',
-//         width: 270
-//       },
-//       {
-//         label: 'Birth  Year',
-//         field: 'birth_year',
-//         sort: 'asc',
-//         width: 100
-//       },
-//       {
-//         label: 'Eye Color',
-//         field: 'eye_color',
-//         sort: 'asc',
-//         width: 100
-//       },
-//       {
-//         label: 'Skin Color',
-//         field: 'skin_color',
-//         sort: 'asc',
-//         width: 100
-//       },
-//       {
-//         label: 'Height',
-//         field: 'height',
-//         sort: 'asc',
-//         width: 100
-//       },
-//       {
-//         label: 'Mass',
-//         field: 'mass',
-//         sort: 'asc',
-//         width: 100
-//       },
-//       {
-//         label: 'Films Acted',
-//         field: 'films',
-//         sort: 'asc',
-//         width: 100
-//       },
-//       {
-//         label: 'No of Species',
-//         field: 'species',
-//         sort: 'asc',
-//         width: 100
-//       },
-//       {
-//         label: 'No of Starships',
-//         field: 'starships',
-//         sort: 'asc',
-//         width: 100
-//       },
-//       {
-//         label: 'No of Vehicles',
-//         field: 'vehicles',
-//         sort: 'asc',
-//         width: 100
-//       }
-//     ],
-//     rows: [
-//       {
-//         name: props.name,
-//         birth_year: props.birthYear,
-//         eye_color: props.eyeColor,
-//         skin_color: props.skinColor,
-//         height: props.height,
-//         mass: props.mass,
-//         films: props.films,
-//         species: props.species,
-//         starships: props.starships,
-//         vehicles: props.vehicles
-//       }
-//     ]
-//   };
+  const data = {
+    columns: [
+      {
+        label: 'Name',
+        field: 'name',
+        sort: 'asc',
+        width: 270
+      },
+      {
+        label: 'Birth  Year',
+        field: 'birth_year',
+        sort: 'asc',
+        width: 100
+      },
+      {
+        label: 'Eye Color',
+        field: 'eye_color',
+        sort: 'asc',
+        width: 100
+      },
+      {
+        label: 'Skin Color',
+        field: 'skin_color',
+        sort: 'asc',
+        width: 100
+      },
+      {
+        label: 'Height',
+        field: 'height',
+        sort: 'asc',
+        width: 100
+      },
+      {
+        label: 'Mass',
+        field: 'mass',
+        sort: 'asc',
+        width: 100
+      },
+      {
+        label: 'Films Acted',
+        field: 'films',
+        sort: 'asc',
+        width: 100
+      },
+      {
+        label: 'No of Species',
+        field: 'species',
+        sort: 'asc',
+        width: 100
+      },
+      {
+        label: 'No of Starships',
+        field: 'starships',
+        sort: 'asc',
+        width: 100
+      },
+      {
+        label: 'No of Vehicles',
+        field: 'vehicles',
+        sort: 'asc',
+        width: 100
+      }
+    ],
+    rows:props.character
+    
+  };
+  console.log(props.character)
+  return (
+    <MDBDataTable
+      striped
+      bordered
+      hover
+      data={data}
+    />
+  );
+}
 
-//   return (
-//     <MDBDataTable
-//       striped
-//       bordered
-//       hover
-//       data={data}
-//     />
-//   );
-// }
-
-// export default DatatablePage;
+export default DatatablePage;
--- starwars/src/components/dataComponent/Contents.js
+++ starwars/src/components/dataComponent/Contents.js
@@ -1,18 +1,30 @@
 import React, { useState, useEffect } from "react";
-import Example from "./ContentTable";
+import DatatablePage from "./ContentTable";
 import axios from "axios";
 
 export default function Contents() {
   const [starWar, setStarWar] = useState([]);
   const [prevstarWar, setPrevStarWar] = useState("");
   const [nxtstarWar, setNxtStarWar] = useState("");
-
   useEffect(() => {
     axios.get(`https://swapi.co/api/people/`)
     .then(response=>{
         const result=response.data;
         console.log(result);
-        const data= result.results;
+        const data= result.results.map(obj=>{
+            return{
+                name:obj.name,
+                skin_color:obj.skin_color,
+                mass:obj.mass,
+                eye_color:obj.eye_color,
+                birth_year:obj.birth_year,
+                height:obj.height,
+                species:obj.species,
+                films:obj.films.length,
+                starships:obj.starships.length,
+                vehicles:obj.vehicles.length
+            }
+        });
         console.log(data)
         setStarWar(data)
         const next=response.data.next;
@@ -30,12 +42,13 @@ export default function Contents() {
  
   return (
       
-        starWar.map(item => {
-          return (<tr> <Example key={item.name} name={item.name} skinColor={item.skin_color} films={item.films.length} mass={item.mass} eyeColor={item.eye_color} birthYear={item.birth_year} height={item.height} species={item.species.length} starships={item.starships.length} vehicles={item.vehicles.length} 
-           />;
-           </tr>)
-        })
-      
-  );
+        
+      <DatatablePage  
+           character={starWar}
+           />
+           
+    
+  )
+  
 
 }
